f09658f5a1660813eab118c0763ecf10586f6fa3
Anatolii Iumashev <anatolii.iumashev@gmail.com>
2025-08-13 14:46:17 +0300

    dddd

--- index.php
+++ index.php
@@ -1,12 +1,20 @@
 <?php
+if (function_exists('opcache_reset')) {
+    // Reset the entire OPcache
+    opcache_reset();
+    echo "OPcache cleared successfully.";
+} else {
+    echo "OPcache is not enabled.";
+}
+clearstatcache();
 
-echo "Hello, World!" . PHP_EOL;
-echo "Hello, World! 2" . PHP_EOL;
-echo "Hello, World! 3" . PHP_EOL;
-echo "Hello, World! 4" . PHP_EOL;
+echo "Hello, World!".PHP_EOL;
+echo "Hello, World! 2".PHP_EOL;
+echo "Hello, World! 3".PHP_EOL;
+echo "Hello, World! 4".PHP_EOL;
 
 $env_test = getenv($_GET['key'] ?? 'APP_KEY');
-echo "env_test: " . $env_test;
+echo "env_test: ".$env_test;
 
 echo '<pre>';
 echo 'getenv():';
