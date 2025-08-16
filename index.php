23239cfe8decb9be3ea24f6bdc4a194ec753c0f9
Anatolii Iumashev <anatolii.iumashev@gmail.com>
2025-08-16 14:29:23 +0300

    .

--- index.php
+++ index.php
@@ -2,15 +2,15 @@
 
 echo "Hello, World!".PHP_EOL;
 
-$env_test = getenv($_GET['key'] ?? 'APP_KEY');
-echo "env_test: ".$env_test;
+// $env_test = getenv($_GET['key'] ?? 'APP_KEY');
+// echo "env_test: ".$env_test;
 
-echo '<pre>';
-echo 'getenv():';
-var_dump(getenv());
-echo '$_ENV:';
-var_dump($_ENV);
-echo '$_SERVER:';
-var_dump($_SERVER);
+// echo '<pre>';
+// echo 'getenv():';
+// var_dump(getenv());
+// echo '$_ENV:';
+// var_dump($_ENV);
+// echo '$_SERVER:';
+// var_dump($_SERVER);
 
 phpinfo();
